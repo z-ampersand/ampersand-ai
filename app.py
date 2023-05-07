1ed4e37658bb3e0235a8de2b5f51739a8f55b41e
z-ampersand <132397903+z-ampersand@users.noreply.github.com>
2023-05-07 18:51:16 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ import streamlit as st
 import datetime
 import os
 import openai
-from helpers import openai_call
+from helpers import openai_call, openai_idea_clusters, openai_character_profiles, openai_movie_timeline
 from streamlit_ws_localstorage import injectWebsocketCode, getOrCreateUID
 from streamlit_timeline import timeline
+from glob import glob
+import json
 # from streamlit_card import card
 from constants import EXAMPLE_PROFILE, EXAMPLE_TIMELINE
 st.set_page_config(
@@ -14,23 +16,19 @@ st.set_page_config(
     layout='wide',
     page_icon="&.ai"
 )
+import plotly.express as px
+# from streamlit_plotly_events import plotly_events
 
-@st.cache_data
-def setup():
-    conn = injectWebsocketCode(hostPort='linode.liquidco.in', uid=getOrCreateUID())
-    return conn.getLocalStorageVal(key='k1')
+conn = injectWebsocketCode(hostPort='linode.liquidco.in', uid=getOrCreateUID())
 
+# @st.cache_data
+# def setup():
+#     conn = injectWebsocketCode(hostPort='linode.liquidco.in', uid=getOrCreateUID())
+#     return conn.getLocalStorageVal(key='k1')
 
-def main():
-    col1, _ = st.columns([1,6])
-
-    col1.title(':blue[_&_].', anchor='&.')
-
-    current_time = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
-    # Main call to the api, returns a communication object
-    ret = setup()
+def login_logic():
+    ret = conn.getLocalStorageVal(key='k1')
     needs_to_create_user_account = len(str(ret))==0
-
     if needs_to_create_user_account:
         col1, col2 = st.columns([4,1])
         user_nom = col1.text_input('select your user name:')
@@ -42,7 +40,6 @@ def main():
             #     st.warning('USER NAME "'+str(user_nom)+'" IS ALREADY TAKEN.')
             # else:        
             # st.warning('setting into localStorage')
-            conn = injectWebsocketCode(hostPort='linode.liquidco.in', uid=getOrCreateUID())
             ret1 = conn.setLocalStorageVal(key='k1', val=user_nom)
             # st.warning(ret1)
             if 'success' not in str(ret1):
@@ -50,16 +47,85 @@ def main():
             else:  
                 needs_to_create_user_account = False     
             is_clicked = False
+        elif is_clicked and len(user_nom) == 0:
+            st.warning('must add user name above')
     else:
         wow1, wow2 = st.columns([5,1])
         wow1.success('Welcome back '+ ret+'! 🎬📝', icon='💡')
         is_signedout = wow2.button('sign out')
         if is_signedout:
-            conn = injectWebsocketCode(hostPort='linode.liquidco.in', uid=getOrCreateUID())
+            # conn = injectWebsocketCode(hostPort='linode.liquidco.in', uid=getOrCreateUID())
             ret1 = conn.setLocalStorageVal(key='k1', val='')
             needs_to_create_user_account = True 
             user_nom = ""
         user_nom = str(ret)
+    return user_nom, needs_to_create_user_account
+
+def make_sidebar():
+    user_nom = ''
+    st.sidebar.title("&. Settings")
+    if 'model_name' not in st.session_state:
+        st.session_state['model_name'] = []
+    if 'cost' not in st.session_state:
+        st.session_state['cost'] = []
+    if 'total_tokens' not in st.session_state:
+        st.session_state['total_tokens'] = []
+    if 'total_cost' not in st.session_state:
+        st.session_state['total_cost'] = 0.0
+
+    model_name = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"), horizontal=True)
+    counter_placeholder = st.sidebar.empty()
+    counter_placeholder.write(f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+    # clear_button = st.sidebar.button("Clear Conversation", key="clear")
+    modes = ['ms.j', 'mr.k', 'mr.z']
+    mode = modes[0]
+    # col3.write('')
+    # col3.write('')
+    doveride = st.sidebar.radio('user name override:', [True, False], index=1, horizontal=True)
+    if doveride:
+        ffs = [x.split('_')[0] for x in glob('*_database.csv')]
+        user_nom = st.sidebar.selectbox('dataset:', ffs)
+    with st.sidebar.expander('settings'):
+        col22, col3 = st.columns([1,1])
+        mode = col3.radio('mode:', modes)
+        OPENAI_API_KEY = col22.text_input('OPENAI_API_KEY', '')
+        if OPENAI_API_KEY!='' and len(OPENAI_API_KEY) > 4:
+            os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
+            openai.api_key = OPENAI_API_KEY
+
+    return mode, user_nom
+
+def load_dataframe_json_from_csv(ff):
+    df = pd.read_csv(ff, header=None)
+    df.columns = ['prompt', 'unix_timestamp', 'result'] 
+    most_recent_ideamap = df['result'].values[-1]
+    most_recent_ideamap = str(most_recent_ideamap)
+    flipped = most_recent_ideamap[::-1]
+    x = most_recent_ideamap.index('[')
+    xend = -(flipped.index(']'))
+    # st.write(x, xend)
+    if xend != 0:
+        st.sidebar.write('ideamap indexes:', x, xend)
+        most_recent_ideamap = most_recent_ideamap[x:xend]
+    try:
+        dd = json.loads(most_recent_ideamap)
+    except:
+        st.warning(ff + ' json decode failed!')
+        dd = {} 
+    return df, dd
+
+def main():
+    mode, user_nom = make_sidebar()
+    col1, _ = st.columns([1,6])
+    col1.title(':blue[_&_].', anchor='&.')
+
+    current_time = datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+    # Main call to the api, returns a communication object
+    # ret = setup()
+    needs_to_create_user_account = False
+    if user_nom == '':
+        user_nom, needs_to_create_user_account = login_logic()
+
 
 
     # st.title('A title with _italics_ :blue[colors] and emojis :sunglasses:')
@@ -68,70 +134,124 @@ def main():
     # col2.write('')
     # col2.write(current_time)
     if not needs_to_create_user_account:
-        modes = ['ms.j', 'mr.k', 'mr.z']
-        mode = modes[0]
-        # col3.write('')
-        # col3.write('')
-        with st.expander('settings'):
-            col22, col3 = st.columns([1,1])
-            mode = col3.radio('mode:', modes)
-            OPENAI_API_KEY = col22.text_input('OPENAI_API_KEY', '')
-            if OPENAI_API_KEY!='' and len(OPENAI_API_KEY) > 4:
-                os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
-                openai.api_key = OPENAI_API_KEY
 
-        tabs = st.tabs(['chat', 'history', 'outline', 'profiles'])
+
+        tabs = st.tabs(['chat', 'history', 'ideation', 'outline', 'profiles'])
         database_df = pd.DataFrame()
         with tabs[0]:
-            if os.environ.get('OPENAI_API_KEY', None):
-                openai_call(user_nom, mode)
-            else:
-                st.warning('please update `OPENAI_API_KEY` in the settings')
-
             if os.path.exists(user_nom+'_database.csv'):
                 database_df = pd.read_csv(user_nom+'_database.csv', header=None)
                 database_df.columns = ['prompt', 'mode', 'unix_timestamp', '&.ai']
                 database_df.index = pd.to_datetime(database_df['unix_timestamp']*1e9)
                 database_df_to_download = database_df.to_csv().encode('utf-8')
-                col22.download_button(
+                st.sidebar.download_button(
                     label="download database [bytes="+str(len(database_df))+"]",
                     data=database_df_to_download,
                     file_name='database.csv',
                     mime='text/csv',
                 )
+            if os.environ.get('OPENAI_API_KEY', None):
+                openai_call(user_nom, mode, database_df)
+            else:
+                st.warning('please update `OPENAI_API_KEY` in the settings')
 
         with tabs[1]:
             st.dataframe(database_df)
+            # hist_json = database_df[['prompt', '&.ai']].T.to_json()
+            # st.json(hist_json)
+        
+        idea_file = user_nom+'_ideation.csv'
+        idea_df = None
         with tabs[2]:
-            do_update1 = st.button('update',key='outline-update')
-            timeline_file = user_nom+'_timeline.json'
-            data = None
-            if os.path.exists(timeline_file):
-                # load data
-                with open(timeline_file, "r") as f:
-                    data = f.read()
-                # render timeline
-            else:
-               data =  EXAMPLE_TIMELINE
-            timeline(data, height=800)
+            do_update1 = st.button('update',key='idea-update')
+            if do_update1:
+                    dat = database_df[['prompt', '&.ai']].T.to_json()
+                    openai_idea_clusters(user_nom, dat)
+                    do_update1 = False
+            if os.path.exists(idea_file):
+                df, dd = load_dataframe_json_from_csv(idea_file)
+            tabs2 = st.tabs(['summary', 'chart'])
+            idea_df = pd.DataFrame()
+
+            with tabs2[0]:
+                if dd:
+                    idea_df = pd.concat([pd.DataFrame(x) for x in dd])
+                    idea_df = pd.concat([pd.DataFrame(x) for x in dd])
+                    idea_df['promptLevel'] = idea_df['prompts'].apply(lambda x: sum(x)/len(x))
+                    idea_df['promptTime'] = pd.to_datetime(idea_df['promptLevel']*1e6)
+                    idea_df['y'] = idea_df['heat_score']**2
+                    for x in idea_df['information'].values:
+                        st.write('> ' + x)
+            with tabs2[1]:
+                plotly_st, = st.columns(1)
+                with st.expander('raw data'):
+                    st.dataframe(df)
+                    st.json(dd, expanded=False)
+                    st.dataframe(idea_df)
+                if dd:
+                    fig = px.scatter(idea_df, x="promptTime", y="y",
+                    size="heat_score", color="heat_score",
+                        hover_name="information", 
+                        #  log_x=True,
+                        size_max=60)
+                        # selected_points = plotly_events(fig)
+                    plotly_st.plotly_chart(fig, True)
+                # if len(selected_points):
+                #     st.dataframe(pd.concat([pd.DataFrame.from_dict(a,orient='index') for a in selected_points]))
+
 
         with tabs[3]:
-            do_update2 = st.button('update',key='profile-update')
-
-            profiles = EXAMPLE_PROFILE
-            col1, _, col2 = st.columns([4,1,4])
-            for i,x in enumerate(profiles['characters']):
-                if i % 2 == 0:
-                    col1.header(x['name'])
-                    col1.slider('fluidity', key='char-'+str(i), value=5, min_value=0, max_value=10)
-                    col1.write(x['description'])
-                    col1.write(' ')
+            if os.path.exists(idea_file):
+                do_update1 = st.button('update',key='outline-update')
+                if do_update1:
+                    dat = '. '.join(idea_df['information'].values.tolist())
+                    openai_movie_timeline(user_nom, dat)
+                    do_update1 = False
+                timeline_file = user_nom+'_timeline.csv'
+                data = None
+                if os.path.exists(timeline_file):
+                    df, data = load_dataframe_json_from_csv(timeline_file)
+                    st.json(data, expanded=False)
+                    # load data
+                    # with open(timeline_file, "r") as f:
+                    #     data = f.read()
+                    # render timeline
                 else:
-                    col2.header(x['name'])
-                    col2.slider('fluidity', key='char-'+str(i), value=9, min_value=0, max_value=10)
-                    col2.write(x['description'])
-                    col2.write(' ')
+                    data =  EXAMPLE_TIMELINE
 
+                if isinstance(data, list) or data.get('events', None):
+                    data = {'events': data}
+                timeline(data, height=800)
+
+        with tabs[4]:
+            profiles_file = user_nom+'_profiles.csv'
+            if os.path.exists(idea_file):
+                do_update2 = st.button('update',key='profile-update')
+                if do_update2:
+                    dat = '. '.join(idea_df['information'].values.tolist())
+                    openai_character_profiles(user_nom, dat)
+                    do_update2 = False
+                profiles = None
+                if os.path.exists(profiles_file):
+                    # profiles = EXAMPLE_PROFILE['characters']
+                    df, dd = load_dataframe_json_from_csv(profiles_file)
+                    profiles = dd
+                st.json(profiles, expanded=False)
+                if profiles is not None:
+                    col1, _, col2 = st.columns([4,1,4])
+                    for i,x in enumerate(profiles):
+                        if i % 2 == 0:
+                            col1.header(x['name'])
+                            col1.slider('fluidity', key='char-'+str(i), value=5, min_value=0, max_value=10)
+                            col1.write(x['description'])
+                            col1.write(' ')
+                        else:
+                            col2.header(x['name'])
+                            col2.slider('fluidity', key='char-'+str(i), value=9, min_value=0, max_value=10)
+                            col2.write(x['description'])
+                            col2.write(' ')
+                else:
+                    st.write('click update to build profiles')
             # if do_update2:
 
 
--- helpers.py
+++ helpers.py
@@ -4,50 +4,149 @@ import streamlit as st
 import os
 import csv
 
-def call_openai_gpt(user_prompt, mode='ms.j'):
+def call_openai_gpt(user_prompt, mode='ms.j', history=None):
     user_prompt = str(user_prompt)
     context = ""
     if '?' not in user_prompt:
         user_prompt+='?'
 
     if mode=='ms.j':
-        preamble = 'You are an inspiring writing partner for a high quality MOVIE trying to extract creativity with probing questions. Answer and ask questions back in a helpful, encouraging style. Respond to this: ' 
+        preamble = 'You are an inspiring writing partner for a high quality MOVIE trying to extract creativity with probing questions. Answer and ask questions back in a helpful, encouraging style.' 
     elif mode=='mr.k':
-        preamble = 'You are a creative writing partner for a high quality MOVIE  trying to extract creativity with probing questions. Answer and ask questions back in an artistic, high brow style. Respond to this: ' 
+        preamble = 'You are a creative writing partner for a high quality MOVIE  trying to extract creativity with probing questions. Answer and ask questions back in an artistic, high brow style.' 
     elif mode=='mr.z':
-        preamble = 'You are an intelligent writing partner for a high quality MOVIE trying to extract creativity with probing questions. Answer and ask questions back in dry wit, cryptic, and skeptical style. Respond to this: ' 
+        preamble = 'You are an intelligent writing partner for a high quality MOVIE trying to extract creativity with probing questions. Answer and ask questions back in dry wit, cryptic, and skeptical style.' 
 
-    user_prompt = preamble + user_prompt
+    # user_prompt = preamble + user_prompt
+
+    all_messages = [
+        {
+        "role": "system", "content": preamble
+        },
+        
+        ]
+    if history is not None and len(history): # assume pd.DataFrame
+        for key, x in history.T.to_dict().items():
+            print(x)
+            all_messages.append({"role": "user", "content": x['prompt']})
+            all_messages.append({"role": "assistant", "content": x['&.ai']})
+    
+    all_messages.append({"role": "user", "content": context + user_prompt})
+    print(all_messages)
     completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo", 
-    messages=[{"role": "user", "content": context + user_prompt}]
+    messages= all_messages
     )
+    calc_cost(completion.get("usage"))
 
     return completion
-     
 
-def openai_call(user_nom, mode): 
+def calc_cost(usage: dict) -> None:
+    total_tokens = usage.get("total_tokens")
+    prompt_tokens = usage.get("prompt_tokens")
+    completion_tokens = usage.get("completion_tokens")
+    st.session_state.total_tokens.append(total_tokens)
+    # pricing logic: https://openai.com/pricing#language-models
+    if st.session_state.model_name == "gpt-3.5-turbo":
+        cost = total_tokens * 0.002 / 1000
+    else:
+        cost = (prompt_tokens * 0.03 + completion_tokens * 0.06) / 1000
+    st.session_state['total_cost'] += cost
+
+def openai_data_call(outfile, the_ask, data):
+    all_messages = []
+    og_user_prompt = str(data) + ' \n ' + the_ask
+    all_messages.append({'role':'system', 'content': 'Only response in JSON format / copyable code.'})
+    all_messages.append({"role": "user", "content": og_user_prompt})
+    completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo", 
+    messages= all_messages
+    )
+
+    response = completion.choices[0].message.content
+    calc_cost(completion.get("usage"))
+    # st.swrite(str(response))
+    st.sidebar.code('OpenAI response:')
+    st.sidebar.json(completion, expanded=False)
+    with open(outfile,'a', newline='') as f:
+        writer = csv.writer(f)
+        cont = str(completion.choices[0].message.content)
+        createtime = (completion.created)
+        writer.writerow([og_user_prompt, createtime, cont]) 
+
+def openai_idea_clusters(user_nom, data):
+    the_ask = 'summarize above ideas. create N clusters of M pieces of information here, each with annotated heat scores. format it in python array like [[{"heat_score": 0-100, "information":"str", "prompts":[1683401273000, ...]}, ...], [], ...]'
+    outfile = user_nom+'_ideation.csv'
+    openai_data_call(outfile, data, the_ask)
+
+
+def openai_movie_timeline(user_nom, data):
+    the_ask =   """write a timeline for a movie using the information above, mimicking this formating scheme:
+      {    
+    "events": [
+      {
+        "media": {
+          "url": "",
+          "caption": ""
+        },
+        "start_date": {
+          "year": "2023"
+        },
+        "text": {
+          "headline": "",
+          "text": ""
+        }
+      },
+      ...
+      ]
+      } 
+    """
+    outfile = user_nom+'_timeline.csv'
+    openai_data_call(outfile, data, the_ask)
+
+def openai_character_profiles(user_nom, data):
+    the_ask =   """  you are a creative writing partner for a new movie. using the above information (be loose with the designs and maximize dramatic effect), build character profiles in JSON format like this:  {
+    "characters": [
+        {
+        "name": "",
+        "description": "",
+        "picture_url": ""
+        },
+    ]}
+    """
+    outfile = user_nom+'_profiles.csv'
+    openai_data_call(outfile, data, the_ask)
+
+
+
+def openai_call(user_nom, mode, history=None): 
     response  = ""
-    col1, col2 = st.columns([6,2])
-    user_prompt = col1.text_area('')
-    col2.write('')
-    col2.write('')
-    is_clicked = col2.button('submit')  
-    og_user_prompt = str(user_prompt)
+    with st.container():
+        chatconvo, yourconvo  = st.columns(2)
 
-    completion = ""
-    if is_clicked and len(user_prompt):
-        completion = call_openai_gpt(user_prompt, mode) 
-        is_clicked = False
+    if 'something' not in st.session_state:
+        st.session_state.something = ''
 
-    if completion:
-        response = completion.choices[0].message.content
-        st.write('> ' + str(response))
-        st.json(completion, expanded=False)
-        with open(user_nom+'_database.csv','a', newline='') as f:
-            writer = csv.writer(f)
-            cont = str(completion.choices[0].message.content)
-            createtime = (completion.created)
-            writer.writerow([og_user_prompt, mode, createtime, cont])
+    def submit():
+        st.session_state.something = st.session_state.widget
+        st.session_state.widget = ''
+        og_user_prompt = str(st.session_state.something)
+        yourconvo.write(og_user_prompt)
+        chatconvo.write(' '*len(og_user_prompt)+'\n')
+        # st.warning(st.session_state.something)
+        if len(og_user_prompt):
+            completion = call_openai_gpt(st.session_state.something, mode, history)
+            response = completion.choices[0].message.content
+            chatconvo.write(str(response))
+            yourconvo.write(' '*len(response)+'\n')
+            st.sidebar.json(completion, expanded=False)
+            with open(user_nom+'_database.csv','a', newline='') as f:
+                writer = csv.writer(f)
+                cont = str(completion.choices[0].message.content)
+                createtime = (completion.created)
+                writer.writerow([og_user_prompt, mode, createtime, cont]) 
+    col1,_ = st.columns([10,1])
+    col1.text_area('', height=2, key='widget', on_change=submit)
+        
 
 
